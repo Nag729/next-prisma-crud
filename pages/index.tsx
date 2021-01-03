f0ee8c025749322f3d4955f13da7b72eeee7dfbd
Nag729 <nagamaaaaachi@gmail.com>
2021-01-03 18:20:50 +0900

    fix top-page design.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
-import { Box, Heading, Image, Link, Text } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Heading,
+  Image,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
 import styles from "../styles/Home.module.css";
 import LinkButton from "./../components/LinkButton";
 
@@ -7,7 +16,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         {/* Nextjs logo */}
-        <Box my="2">
+        <Box mb="2">
           <Image
             borderRadius="full"
             boxSize="150px"
@@ -18,12 +27,11 @@ export default function Home() {
 
         {/* title */}
         <Heading
-          maxWidth="50rem"
+          maxWidth="90vw"
           size="3xl"
-          mt={5}
-          mb={5}
+          my="5"
           textAlign="center"
-          color="gray.800"
+          color="gray.700"
         >
           Welcome to{" "}
           <Link href="https://nextjs.org/" color="blue.500" isExternal>
@@ -37,36 +45,52 @@ export default function Home() {
         </Heading>
 
         {/* description */}
-        <Box mt="3">
-          <Text fontSize="xl">
-            アプリを実践的なものにするために{" "}
-            <Link href="https://chakra-ui.com/" color="teal.500" isExternal>
-              chakra
-            </Link>
-            {" と "}
-            <Link href="https://jestjs.io/ja/" color="teal.500" isExternal>
-              Jest
-            </Link>{" "}
-            を利用しています。
-          </Text>
-          <Text fontSize="xl">
-            キャッチアップのための情報は、
-            <Link href="https://qiita.com/" color="teal.500" isExternal>
-              Qiita - xxx
-            </Link>{" "}
-            にまとめています。
-          </Text>
+        <Box my="3" maxWidth="80vw">
+          <List spacing={2}>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="teal.500" />
+              <Link href="https://nextjs.org/" color="teal.500" isExternal>
+                Next.js
+              </Link>{" "}
+              と{" "}
+              <Link href="https://www.prisma.io/" color="teal.500" isExternal>
+                Prisma
+              </Link>{" "}
+              を利用して、スターバックスの カスタマイズドリンク を CRUD
+              できるサンプルアプリです。
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="teal.500" />
+              アプリを実践的なものにするために{" "}
+              <Link href="https://chakra-ui.com/" color="teal.500" isExternal>
+                chakra
+              </Link>
+              {" と "}
+              <Link href="https://jestjs.io/ja/" color="teal.500" isExternal>
+                Jest
+              </Link>{" "}
+              を利用しています。
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="teal.500" />
+              キャッチアップのための情報は、
+              <Link href="https://qiita.com/Nag729" color="teal.500" isExternal>
+                Qiita - xxx
+              </Link>{" "}
+              にまとめています。
+            </ListItem>
+          </List>
         </Box>
 
         {/* link button */}
-        <Box mt={5} mb={5}>
+        <Box my="3">
           {/* Create */}
-          <Box mt={5} mb={5}>
+          <Box my="6">
             <LinkButton href="/create">Create New Drink</LinkButton>
           </Box>
 
           {/* Link */}
-          <Box mt={5} mb={5}>
+          <Box my="6">
             <LinkButton href="/list">Go To Drink List</LinkButton>
           </Box>
         </Box>
