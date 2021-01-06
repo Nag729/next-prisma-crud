a0a728532a496a01639083a512177e01e04cd381
Nag729 <nagamaaaaachi@gmail.com>
2021-01-06 22:36:29 +0900

    add jest to project.

---
+++ components/__tests__/BackToHome.spec.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import BackToHome from "../BackToHome";
+
+test("BackToHome", () => {
+  const component = renderer.create(<BackToHome />);
+
+  // TODO: テスト書いていく
+});
